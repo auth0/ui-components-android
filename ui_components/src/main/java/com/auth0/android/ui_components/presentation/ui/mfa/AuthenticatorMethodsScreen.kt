25d8f414ffa1be3801ad8d8c3e1fadfe935d63fc
Prince Mathew <prince.mathew@okta.com>
2026-01-14 16:00:25 +0530

    Passkey screen for initiating passkey enrollment flow

--- ui_components/src/main/java/com/auth0/android/ui_components/presentation/ui/mfa/AuthenticatorMethodsScreen.kt
+++ ui_components/src/main/java/com/auth0/android/ui_components/presentation/ui/mfa/AuthenticatorMethodsScreen.kt
@@ -1,26 +1,30 @@
 package com.auth0.android.ui_components.presentation.ui.mfa
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.auth0.android.ui_components.R
 import com.auth0.android.ui_components.di.MyAccountModule
 import com.auth0.android.ui_components.presentation.ui.components.CircularLoader
 import com.auth0.android.ui_components.presentation.ui.components.ErrorHandler
 import com.auth0.android.ui_components.presentation.ui.components.TopBar
-import com.auth0.android.ui_components.presentation.ui.mfa.authenticator_methods.AuthenticatorListScreen
+import com.auth0.android.ui_components.presentation.ui.mfa.authenticator_methods.PrimaryAuthenticatorListScreen
+import com.auth0.android.ui_components.presentation.ui.mfa.authenticator_methods.SecondaryAuthenticatorListScreen
 import com.auth0.android.ui_components.presentation.viewmodel.AuthenticatorMethodsViewModel
 import com.auth0.android.ui_components.presentation.viewmodel.AuthenticatorUiData
 import com.auth0.android.ui_components.presentation.viewmodel.AuthenticatorUiState
@@ -78,8 +82,16 @@ fun AuthenticatorMethodsScreen(
                 }
 
                 is AuthenticatorUiState.Success -> {
-                    Column {
-                        AuthenticatorListScreen(state.data, onAuthenticatorItemClick)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState()),
+                    ) {
+//                        PrimaryAuthenticatorListScreen()
+                        SecondaryAuthenticatorListScreen(
+                            authenticatorMethodList = state.data,
+                            onAuthenticatorItemClick = onAuthenticatorItemClick
+                        )
                     }
                 }
             }
