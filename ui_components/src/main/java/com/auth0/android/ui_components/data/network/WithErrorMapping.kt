f01f78bd1fa2bfba4741c850a1b53fa8cb920ef3
Prince Mathew <prince.mathew@okta.com>
2026-03-10 12:36:43 +0530

    refactor : Rethrows cancellation exception and added error case for steup scenario

--- ui_components/src/main/java/com/auth0/android/ui_components/data/network/WithErrorMapping.kt
+++ ui_components/src/main/java/com/auth0/android/ui_components/data/network/WithErrorMapping.kt
@@ -1,12 +1,15 @@
 package com.auth0.android.ui_components.data.network
 
 import com.auth0.android.ui_components.data.mapper.ErrorMapper
+import kotlin.coroutines.cancellation.CancellationException
 
 
 /**
- * Executes a suspend request and maps exceptions to Auth0Error
+ * Executes a suspend request and maps exceptions to Auth0Error.
+ * Rethrows [CancellationException] to preserve structured concurrency.
  *
  * @param execute The suspend block to execute
+ * @throws [CancellationException] if the coroutine is cancelled
  * @throws [com.auth0.android.ui_components.domain.error.Auth0Error]
  */
 internal suspend inline fun <reified T> withErrorMapping(
@@ -15,6 +18,8 @@ internal suspend inline fun <reified T> withErrorMapping(
 ): T {
     return try {
         execute()
+    } catch (e: CancellationException) {
+        throw e
     } catch (e: Throwable) {
         throw ErrorMapper.mapToAuth0Error(e, scope)
     }
